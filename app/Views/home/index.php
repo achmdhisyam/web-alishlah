8cff91b904a24a9949cfa54f51767756ede84da7
achmdhisyam <achmadhisyamuddin@gmail.com>
2026-06-28 14:18:32 +0700

    feat: implement home page modular layout and add development roadmap documentation

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -2,12 +2,12 @@
 include('popup.php');
 include('banner.php');
 include('spmb_menu.php');
+include('about.php');
 include('jenjang.php');
 include('yayasan.php');
 // include('program_pendidikan.php');
 // include('client.php');
 include('keunggulan.php');
-include('about.php');
 include('video.php');
 include('berita.php');
 include('prestasi.php');
